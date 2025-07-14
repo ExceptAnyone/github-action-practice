dd3c78e05991ab09cdb738c7717e87ec5035a664
장정안 <wjddks96@naver.com>
2025-07-14 21:38:21 +0900

    test: 스토리북 온클릭 제거

--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -23,7 +22,6 @@ const meta: Meta<typeof Button> = {
     disabled: { control: 'boolean' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
 };
 
 export default meta;
