7aabc8e95e6b3ac9829cc35f6f3445b3be02531e
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2025-07-15 22:10:40 +0900

    test: 버튼 주석 제거 (#1)
    * test: 버튼 주석 제거
    
    * fix: 네이밍 변경
    
    * feat: 그대로 복붙
    
    * feat: lint state 삭제
    
    * engines 추가

--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -6,12 +6,10 @@ const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    // Optional parameter to cener the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
     size: {
@@ -21,7 +19,6 @@ const meta: Meta<typeof Button> = {
     primary: { control: 'boolean' },
     disabled: { control: 'boolean' },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 };
 
 export default meta;
